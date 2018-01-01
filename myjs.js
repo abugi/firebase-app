165aa9b4eb06fc3334627e0a6ce334e91c57fda2
abugi <abugiboi@gmail.com>
2018-01-01 21:55:41 +0100

    initial commit

--- myjs.js
+++ myjs.js
@@ -5,9 +5,9 @@ function signIn(){
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user.displayName;
+        var user = result.user;
         //getting User specific information
-        console.log(user);
+        console.log(user.displayName);
 
     }).catch(function (error) {
         // Handle Errors here.
