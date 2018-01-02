229f0edbcecdf5c03a9803b1e7fa4dbf44985422
abugi <abugiboi@gmail.com>
2018-01-02 15:00:35 +0100

    initial commit

--- myjs.js
+++ myjs.js
@@ -74,16 +74,16 @@ function uploadFile(){
     uploadTask.on('state_changed', function (snapshot) {
         // Observe state change events such as progress, pause, and resume
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                break;
-        }
+    //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //     console.log('Upload is ' + progress + '% done');
+    //     switch (snapshot.state) {
+    //         case firebase.storage.TaskState.PAUSED: // or 'paused'
+    //             console.log('Upload is paused');
+    //             break;
+    //         case firebase.storage.TaskState.RUNNING: // or 'running'
+    //             console.log('Upload is running');
+    //             break;
+    //     }
     }, function (error) {
         // Handle unsuccessful uploads
     }, function () {
