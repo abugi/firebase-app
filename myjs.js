2631e291e7f591aea97b525817734a5d14925c2a
abugi <abugiboi@gmail.com>
2018-01-02 15:15:31 +0100

    initial commit

--- myjs.js
+++ myjs.js
@@ -4,7 +4,7 @@ var user;
 var selectedFile;
 
 $(document).ready(function(){
-    // $('#welcome').hide();
+    $('#welcome').hide();
     $('#uploadButton').hide();
     $('.upload-group').hide();
 });
@@ -72,20 +72,9 @@ function uploadFile(){
     // 2. Error observer, called on failure
     // 3. Completion observer, called on successful completion
     uploadTask.on('state_changed', function (snapshot) {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //     console.log('Upload is ' + progress + '% done');
-    //     switch (snapshot.state) {
-    //         case firebase.storage.TaskState.PAUSED: // or 'paused'
-    //             console.log('Upload is paused');
-    //             break;
-    //         case firebase.storage.TaskState.RUNNING: // or 'running'
-    //             console.log('Upload is running');
-    //             break;
-    //     }
+        console.log(snapshot)
     }, function (error) {
-        // Handle unsuccessful uploads
+        console.log(error)
     }, function () {
         // Handle successful uploads on complete
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
