506d955e87e1c244d6580cdd67e817b8a22eb4cb
abugi <abugiboi@gmail.com>
2018-01-03 19:57:59 +0100

    initial commit

--- timeline.js
+++ timeline.js
@@ -17,7 +17,7 @@ function queryDatabase(token){
         console.log(posts);
         var objKeys = Object.keys(posts);
         for(var i = 0; i < objKeys.length; i++){
-            console.log(objKeys[i]);
+            var currentObject = posts[objKeys[i]]
         }
     });
 }
